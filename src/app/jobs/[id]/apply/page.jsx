41df4e48d6636a2283c604ff8a3de83d467d3cfd
ProgrammingHero1 <programming.hero1@gmail.com>
2026-06-05 22:43:06 -0500

    set limit on how many job one candidate can apply

--- src/app/jobs/[id]/apply/page.jsx
+++ src/app/jobs/[id]/apply/page.jsx
@@ -3,17 +3,19 @@ import { getUserSession } from '@/lib/core/session';
 import { redirect } from 'next/navigation';
 import React from 'react';
 import JobApply from './JobApply';
+import { getApplicationsByApplicant } from '@/lib/api/applications';
+import Link from 'next/link';
 
 const ApplyPage = async ({ params }) => {
     const { id } = await params;
 
     const user = await getUserSession();
-
-    if(!user) {
+    console.log('Current User Session:', user);
+    if (!user) {
         redirect(`/auth/signin?redirect=/jobs/${id}/apply`);
     }
 
-    if(user.role !== 'seeker') {
+    if (user.role !== 'seeker') {
         return (
             <div className="w-full min-h-screen bg-zinc-950 flex flex-col justify-center items-center text-white p-6">
                 <p className="text-zinc-400 text-lg">Only job seekers can apply for positions. Please sign in with a seeker account to proceed.</p>
@@ -21,12 +23,23 @@ const ApplyPage = async ({ params }) => {
         );
     }
 
+    const applications = await getApplicationsByApplicant(user.id);
+
+    const plan = {
+        name: 'Free',
+        maxApplicationsPerMonth: 3
+    }
     const job = await getJobById(id);
 
 
     return (
         <div>
-            <JobApply applicant={user} job={job} />
+            <h2> You have applied so far: {applications.length} out of {plan.maxApplicationsPerMonth} this month </h2>
+            <p>Purchase plan to apply for more positions. <Link href="/plans">View Plans</Link> </p>
+            {
+                applications.length < plan.maxApplicationsPerMonth && (
+                    <JobApply applicant={user} job={job} />
+                )}
         </div>
     );
 };
